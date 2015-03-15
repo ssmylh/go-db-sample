fc4bec8f787b2ce838bf6e71e72459249e2ebf16
ssmylh <ssmylh@gmail.com>
2015-03-15 09:58:03 +0900

    Add gorp sample

--- README.md
+++ README.md
@@ -16,8 +16,15 @@ go get github.com/mattn/go-sqlite3
 go get github.com/naoina/genmai
 ```
 
+- For gorp sample
+
+```
+go get gopkg.in/gorp.v1
+```
+
 ## References
 
 - [Package sql](http://golang.org/pkg/database/sql/)
 - [go-sqlite3/_example/simple.go](https://github.com/mattn/go-sqlite3/blob/0cdea24bc72fac013abf416f27acd433e5906528/_example/simple/simple.go)
 - [genmai](https://github.com/naoina/genmai)
+- [gorp](https://github.com/go-gorp/gorp)
---
+++ gorp.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"gopkg.in/gorp.v1"
+)
+
+type sample struct {
+	Id   int
+	Misc string
+}
+
+func main() {
+	/* open db */
+	db, err := sql.Open("sqlite3", ":memory:")
+	if err != nil {
+		fmt.Printf("Failed to open db: %s\n", err)
+		return
+	}
+
+	/* initialize DbMap */
+	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.SqliteDialect{}}
+	defer dbmap.Db.Close()
+
+	dbmap.AddTableWithName(sample{}, "sample").SetKeys(false, "Id") // don't use autoincrement
+
+	/* create table */
+	err = dbmap.CreateTablesIfNotExists()
+	if err != nil {
+		fmt.Printf("Failed to create table: %s\n", err)
+		return
+	}
+
+	/* insert */
+	tx, err := dbmap.Begin()
+	if err != nil {
+		fmt.Printf("Failed to begin transaction: %s\n", err)
+		return
+	}
+
+	sample1 := sample{
+		Id:   1,
+		Misc: "sample 1",
+	}
+	sample2 := sample{
+		Id:   2,
+		Misc: "sample 2",
+	}
+	samples := []sample{sample1, sample2}
+	for _, sample := range samples {
+		err = tx.Insert(&sample)
+		if err != nil {
+			fmt.Printf("Failed to insert: %s\n", err)
+			return
+		}
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		fmt.Printf("Failed to commit transaction: %s\n", err)
+		return
+	}
+
+	/* select all */
+	var res1 []sample
+	_, err = dbmap.Select(&res1, "select * from sample")
+	if err != nil {
+		fmt.Printf("Failed to select: %s\n", err)
+		return
+	}
+	for _, sample := range res1 {
+		fmt.Printf("id: %d, misc: %s\n", sample.Id, sample.Misc)
+	}
+
+	/* select one */
+	obj, err := dbmap.Get(sample{}, 1)
+	if err != nil {
+		fmt.Printf("Failed to select by id: %s\n", err)
+	}
+	sample := obj.(*sample)
+	fmt.Printf("id: %d, misc: %s\n", sample.Id, sample.Misc)
+}
